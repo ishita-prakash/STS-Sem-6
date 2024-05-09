2f4d90af69559abc256bd3c8c860d74e3fa3ee1e
Ishita Prakash <136169172+ishita-prakash@users.noreply.github.com>
2024-05-09 11:46:38 +0530

    Update hset_lhset_tset.java

--- hset_lhset_tset.java
+++ hset_lhset_tset.java
@@ -1,3 +1,22 @@
+/* set functions :-
+1. adding elements -> myset.add("apple")
+2. removing elements -> myset.remive("apple")
+3. checking if set contains an element -> boolean containsApple = myset.contains("apple")
+4. size of set -> int n = myset.size()
+5. checking if set is empty -> boolean setEmpty = myset.isEmpty()
+6. iterate over elements -> for (Integer element: myset)
+                                System.out.println(element)
+7. clear/reset a set -> myset.clear()
+8. set union -> create set1, create set2
+                Set<String> unionset = new HashSet<>(set1)
+                unionset.addAll(set2)
+9. set intersection -> create set1, create set2
+                        Set<Integer> intersectionset = new HashSet<>(set1)
+                        intersectionset.retainAll(set2)
+10. set difference -> create set1, create set2
+                        Set<Integer> differenceset = new HashSet<>(set2)
+                        differenceset.removeAll(set2)
+*/
 import java.util.Set;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
